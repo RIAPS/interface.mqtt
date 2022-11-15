367d9c30eedae38dd29403e5c645f79ce7938c07
Scott Eisele <eiselesr@gmail.com>
2022-11-15 21:18:35 +0000

    move app specific message handler to app level component

--- MqttDevice.py
+++ MqttDevice.py
@@ -11,20 +11,12 @@ class MqttDevice(Component):
         self.thread = None
         self.mqtt_config = mqtt_config
 
-    def on_data(self):
+    def handleActivate(self):
         if self.thread is None:  # First clock pulse
             self.thread = RiapsMQThread(self.trigger, self.logger, self.mqtt_config)  # Inside port
             self.thread.start()  # Start
             self.trigger.set_identity(self.thread.get_identity(self.trigger))
             self.trigger.activate()
-        data = self.data.recv_pyobj()  # Receive data from SGen
-        self.logger.info('on_data():%r' % data)
-        msg = {"data": data,
-               "topic": "riaps/data"}
-        self.trigger.send_pyobj(msg)
-        # This puts the message on the plug.
-        # So when `handle_polled_sockets` is called it picks up this message
-        # and publishes it to the broker.
 
     def __destroy__(self):
         self.logger.info("__destroy__")
@@ -34,8 +26,15 @@ class MqttDevice(Component):
             self.thread.join()
         self.logger.info("__destroy__ed")
 
+    def send_mqtt(self, msg: dict):
+        self.trigger.send_pyobj(msg)
+
     @abc.abstractmethod
     def on_trigger(self):  # Internally triggered operation
+        """
+        This is triggered when a message is received from the mqtt broker.
+        Specifically the handle_broker_message function sends it.
+        """
         pass
 
 
